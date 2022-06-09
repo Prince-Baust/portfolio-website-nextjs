5152dee77f74db6b1c33b6ef43562812e47700c9
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-09 16:02:51 +0600

    footer complete and final commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import {AiFillFacebook, AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
@@ -12,7 +12,27 @@ const Footer = () => {
           <LinkTitle>Call</LinkTitle>
           <LinkItem href="tel:01752-276-886">01752-276-886</LinkItem>
         </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>E-mail</LinkTitle>
+          <LinkItem href="mailto:write2maruf@icloud.com">write2maruf@icloud.com</LinkItem>
+        </LinkColumn>
       </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Prince-Baust">
+            <AiFillGithub size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/maruf-prince/">
+            <AiFillLinkedin size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://www.facebook.com/maruf.prince.16">
+            <AiFillFacebook size="3rem"/>
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
     </FooterWrapper>
   );
 };
