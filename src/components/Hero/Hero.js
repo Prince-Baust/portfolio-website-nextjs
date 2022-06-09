daadfdaba0e2f28d0018f8474fad4b3a4dd9910a
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-09 14:35:41 +0600

    projects section complete

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,15 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>Welcome to <br/> My Personal Portfolio</SectionTitle>
+      <SectionText>
+        Engineering student with the ability to review plans, write reports, research solutions and implement company procedures. Willingness to learn, ability to follow instructions and work cooperatively within team environments. Competencies include various frameworks and technologies like MERN stack, Scrum etc.
+      </SectionText>
+      <Button onClick={() => window.location="https://www.linkedin.com/in/maruf-prince/"}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
